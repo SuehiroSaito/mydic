ab99b898cd4c564a6faa6f942682ba8a4511886a
Suehiro Saito <suehiro3721p@gmail.com>
2024-04-04 07:44:20 +0900

    WebWorkerテスト

---
+++ test/smp_WebWorker.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let result = document.getElementById('result');
+  let worker = new Worker('WWPrimes.js');
+
+  document.getElementById('btn').addEventListener('click', function (e) {
+    worker.postMessage({
+      target: document.getElementById('target').value,
+      num: document.getElementById('num').value,
+    });
+    result.textContent = '（計算中...）';
+  });
+
+  worker.addEventListener('message', function (e) {
+    result.textContent = e.data;
+  }, false);
+
+  worker.addEventListener('error', function (e) {
+    window.alert(e.message);
+  }, false);
+}, false);
