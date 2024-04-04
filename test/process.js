c2709ad3f94d14e84980eaf0b63cafe48f6b250b
Suehiro Saito <suehiro3721p@gmail.com>
2024-04-04 11:03:22 +0900

    1103

--- test/process.js
+++ test/process.js
@@ -1,9 +1,12 @@
 self.addEventListener('message', function (e) {
-  console.log("process.js head 0940")
+  console.log("process.js head")
+  num = e.data.num
+  
   let count = 0;
   let target = 100
   for (let i = 1; i < 100; i++) {
     if (i % e.data.num === 0) { count++; }
   }
+
   postMessage(count);
 });
